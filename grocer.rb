c04a5db805ea647d1bb20637db8780f5d9e1dea5
Daniel Lechok <dclechok@gmail.com>
2019-09-05 19:04:24 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,19 @@
 def consolidate_cart(cart)
-  # code here
+  final_hash = {}
+  cart.each do |ele|
+    ele_name = ele.keys[0]
+    # p ele_name
+    if final_hash.has_key?(ele_name)
+      final_hash[ele_name][:count] += 1 
+    else
+      final_hash[ele_name] = { # add to hash and consolidate
+        count: 1, # create count
+        price: ele[ele_name][:price], 
+        clearance: ele[ele_name][:clearance]
+      }
+    end
+  end
+  final_hash
 end
 
 def apply_coupons(cart, coupons)
